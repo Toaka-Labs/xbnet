828950148cbcfd8986355d08c8c3ab2b62be394b
Justin Sutcliff <justins210@gmail.com>
2023-08-13 12:20:09 -0400

    cleanup and additional options, rolling version

--- docker_build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Build image
-docker build -t xbnet:latest -o build .
-
-# Grab the debian package from a temporary container
-# docker cp $(docker create --name tc xbnet):/opt/xbnet/target/debian/xbnet_1.1.0_amd64.deb ./xbnet_1.1.0_amd64.deb && docker rm tc
